57d323507c2c13cc8ee1027cb8141ca094a419c6
Adrien(Poney) <bernard.adrien16@gmail.com>
2023-11-21 21:04:05 +0100

    [Refactor] AddMedicineActivity
    OK

--- pills_me/app/src/main/java/be/helha/pills_me/controllers/AddMedicineActivity.java
+++ pills_me/app/src/main/java/be/helha/pills_me/controllers/AddMedicineActivity.java
@@ -14,51 +14,56 @@ import be.helha.pills_me.models.BankPills;
 import be.helha.pills_me.models.Pill;
 
 public class AddMedicineActivity extends AppCompatActivity {
-
     private EditText mNameOfPills;
     private Button mAddbutton;
     private checkBoxMMEFragment mFragmentController;
     private NumberPicker mNumberPicker;
-    private Pill mCurrentPillCreated;
+
+    private static final int MIN_VALUE = 1;
+    private static final int MAX_VALUE = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pills);
 
+        //Get the fragment which contains the checkboxes
         FragmentManager fragmentManager = getSupportFragmentManager();
         mFragmentController = (checkBoxMMEFragment) fragmentManager.findFragmentById(R.id.fragmentContainerView);
 
         mNameOfPills = findViewById(R.id.name_pill_edit_text);
-        mAddbutton = findViewById(R.id.add_button);
-        mAddbutton.setOnClickListener(view -> {
-            boolean isPillCreate =createPill();
-            if (isPillCreate && mCurrentPillCreated != null){
-                Toast.makeText(this, mCurrentPillCreated.getName()
-                        + " "
-                        + getString(R.string.pillAdded), Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        });
 
         mNumberPicker = findViewById(R.id.day_number_picker);
-        mNumberPicker.setMinValue(0b1);
-        mNumberPicker.setMaxValue(31);
+        mNumberPicker.setMinValue(MIN_VALUE);
+        mNumberPicker.setMaxValue(MAX_VALUE);
 
+        mAddbutton = findViewById(R.id.add_button);
+        mAddbutton.setOnClickListener(view -> {
+            boolean isPillCreate = createPill();
+            if (isPillCreate) {
+                Toast.makeText(this, getString(R.string.pillAdded), Toast.LENGTH_SHORT).show();
+                finish(); //Close the activity
+            }
+        });
     }
-    private boolean createPill(){
-        if(mNameOfPills.getText().toString().equals("")){
+
+    private boolean createPill() {
+        //Check if the name of the pill is not empty
+        if (mNameOfPills.getText().toString().isEmpty()) {
             return false;
         }
 
-        mCurrentPillCreated = new Pill(
-                mNameOfPills.getText().toString(),
-                mNumberPicker.getValue(),
-                mFragmentController.isMorningCheckBoxChecked(),
-                mFragmentController.isMidDayCheckBoxChecked(),
-                mFragmentController.isEveningCheckBoxChecked());
-
-        BankPills.getInstance(this).addPill(mCurrentPillCreated);
-        return true;
+        //Add the pill to the DB
+        try {
+            BankPills.getInstance(this).addPill(new Pill(
+                    mNameOfPills.getText().toString(),
+                    mNumberPicker.getValue(),
+                    mFragmentController.isMorningCheckBoxChecked(),
+                    mFragmentController.isMidDayCheckBoxChecked(),
+                    mFragmentController.isEveningCheckBoxChecked()));
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
